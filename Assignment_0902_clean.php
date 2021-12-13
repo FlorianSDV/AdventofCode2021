74346a60db56a2294d8693050ea23647373ff81a
FlorianSDV <180707@novacollege.nl>
2021-12-13 08:54:48 +0100

    DAY 9 part 2 final touches

--- Assignment_0902_clean.php
+++ Assignment_0902_clean.php
@@ -7,34 +7,23 @@ function search($x, $y) // recursieve functie die alle punten in een pool telt.
     global $array; // global zodat we deze waarden vanuit de functie kunnen aanpassen
     global $tellertje;
     if ($array[(int)$y][(int)$x] != "x") { // check of dit vakje een x is. als het een x is, stopt de functie.
-
         $tellertje++;
         $array[(int)$y][(int)$x] = "x"; // huidige plek naar 'x' veranderen zodat we weten dat we er geweest zijn
-        // check boven
-        search((int)$x, (int)$y - 1);
-        // check rechts
-        search((int)$x + 1, (int)$y);
-        // check onder
-        search((int)$x, (int)$y + 1);
-        // check links
-        search((int)$x - 1, (int)$y);
+        search((int)$x, (int)$y - 1); // check boven
+        search((int)$x + 1, (int)$y); // check rechts
+        search((int)$x, (int)$y + 1); // check onder
+        search((int)$x - 1, (int)$y); // check links
     }
 }
-
 $array = explode(PHP_EOL, (file_get_contents("Assignment_0902_input")));
-
 for ($i = 0; $i < count($array); $i++) { //  zet alles in een array
     $array[$i] = str_split($array[$i], 1);
-}
-
-for ($i = 0; $i < count($array); $i++) {
     for ($j = 0; $j < count($array[$i]); $j++) {
         if ($array[$i][$j] == 9) {
             $array[$i][$j] = "x"; // verander elke 9 naar: 'x'
         }
     }
 }
-
 for ($y = 0; $y < count($array); $y++) { // loop die van pool naar pool loopt
     for ($x = 0; $x < count((array)$array[$y]); $x++) {
         if ($array[$y][$x] != "x") {
@@ -44,7 +33,5 @@ for ($y = 0; $y < count($array); $y++) { // loop die van pool naar pool loopt
         }
     }
 }
-
 rsort($puntenArray); // sorteer de array descending
-
 echo $puntenArray [0] * $puntenArray [1] * $puntenArray [2]; // product van de drie hoogsten als antwoord
